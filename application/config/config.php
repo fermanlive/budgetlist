cdca0d861cce511fe2958566cd1302a3d0c588a5
fermanlive <fernando.link32@gmail.com>
2019-07-10 01:48:30 +0000

    configurador en serivdor

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/budgetlist/';
+$config['base_url'] = 'http://fermanlive.me/budgetlist/';
 
 /*
 |--------------------------------------------------------------------------
